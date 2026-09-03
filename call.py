aae89a0df455bb975393fcdd4be1bbd560a150ac
Paul Crémoux <paul.cremouxguiblain21@gmail.com>
2026-09-03 08:41:44 +0200

    Test

---
+++ call.py
@@ -0,0 +1,5 @@
+from test import hello
+
+hello("Jean")
+if __name__ == "__main__" :
+    print("I'm call.py")
---
+++ tp0.py
@@ -0,0 +1,8 @@
+def hello(name: str) -> None : 
+    print(f"Hello {name} ! ")
+
+print("Hello World ! ")
+print(f"Script name : {__name__}")
+
+if __name__ == "__main__" :
+    hello("Jeune Trublion")
